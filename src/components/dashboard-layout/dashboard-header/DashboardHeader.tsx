7922a74dc540db499dc52956735e139a10f8a7cb
julia snegova <ssnnggvv@gmail.com>
2024-12-06 18:08:38 +0300

    feat: add new components

--- src/components/dashboard-layout/dashboard-header/DashboardHeader.tsx
+++ src/components/dashboard-layout/dashboard-header/DashboardHeader.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import { GlobalLoader } from '@/components/global-loader/GlobalLoader';
+
 import DashboardProfile from './dashboard-profile/DashboardProfile';
 
 export default function DashboardHeader() {
 	return (
 		<header>
+			<GlobalLoader />
 			<DashboardProfile />
 		</header>
 	);
---
+++ src/components/global-loader/GlobalLoader.tsx
@@ -0,0 +1,14 @@
+import { useIsFetching, useIsMutating } from '@tanstack/react-query';
+
+import { Loader } from '../ui/Loader';
+
+export const GlobalLoader = () => {
+	const isMutating = useIsMutating();
+	const isFetching = useIsFetching();
+
+	return isFetching || isMutating ? (
+		<div className='fixed top-layout right-layout z-50'>
+			<Loader />
+		</div>
+	) : null;
+};
