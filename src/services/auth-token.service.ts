9f12aea7d3baa2bf1b7a6fc74c85619d9ec765c3
julia snegova <ssnnggvv@gmail.com>
2024-12-11 14:56:23 +0300

    refactor(cookieOptions): change sameSite option

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -13,7 +13,7 @@ export const getAccessToken = () => {
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
 		domain: process.env.NEXT_PUBLIC_DOMAIN,
-		sameSite: 'strict',
+		sameSite: 'none',
 		expires: 1
 	});
 };
