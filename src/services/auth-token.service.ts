5631fcbd91d06804934b47cb60b646ed85b93d93
julia snegova <ssnnggvv@gmail.com>
2024-12-11 04:19:34 +0300

    add .env.example

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -12,7 +12,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		domain: 'localhost',
+		domain: process.env.NEXT_PUBLIC_DOMAIN,
 		sameSite: 'strict',
 		expires: 1
 	});
